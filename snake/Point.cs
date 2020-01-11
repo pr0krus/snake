ec072944b178aaf0d1d4a31e4da333af55cd7f16
prokrus <prokrus@gmail.com>
2020-01-11 13:50:44 +0300

    0.5

--- snake/Point.cs
+++ snake/Point.cs
@@ -10,17 +10,14 @@ namespace snake
         public int y;
         public char sym;
 
-        public Point()
-        {            
-        }
-
+        
         public Point(int _x, int _y, char _sym)
         {
             x = _x;
             y = _y;
             sym = _sym;
         }
-        public void Draw()
+        public void Draw(Point i)
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
--- snake/Program.cs
+++ snake/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-
-
-
-
+using System.Collections.Generic;
 
 namespace snake
 {
@@ -11,10 +8,29 @@ namespace snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1,3,'*');
-            p1.Draw();
-
+            
             Point p2 = new Point(3,5,'#');
-            p2.Draw();
+            
+            Point p3 = new Point(7, 10, '@');
+            
+            Point p4 = new Point(11, 15, '%');
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            foreach (Point i in pList)
+            {
+                i.Draw(i);
+
+            }
+            
+
+          
+            
+
 
             Console.ReadLine();
         }
